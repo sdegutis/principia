a562d41371d217505a419e18238bea038ed3369f
Steven Degutis <sbdegutis@gmail.com>
2023-07-10 14:00:19 -0500

    Better hello world.

--- app/main.tsx
+++ app/main.tsx
@@ -2,10 +2,14 @@ import { createPersistentServer, makeRequestHandler } from "./core/http-server";
 import { renderElement } from './core/jsx';
 
 let server: ReturnType<typeof createPersistentServer> = (persisted['server'] ??= createPersistentServer(8080));
+
 server.httpHandler = makeRequestHandler(input => {
   return {
     body: Buffer.from(renderElement(<>
-      <b>Hello</b> world
+      <p>
+        <b>Hello</b> world
+      </p>
+      <p>You visited <u>{input.url}</u></p>
     </>)),
   };
 });
